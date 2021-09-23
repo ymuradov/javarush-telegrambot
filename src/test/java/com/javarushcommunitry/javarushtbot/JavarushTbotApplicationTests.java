63958a988d2de6d9d7dac30509d445a6c621da05
yusuf.muradov <yusuf.muradov@physiq.com>
2021-09-23 16:57:58 +0300

    add SpringBoot Skeleton

---
+++ src/test/java/com/javarushcommunitry/javarushtbot/JavarushTbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarushcommunitry.javarushtbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
